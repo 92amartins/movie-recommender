28e34c38f62fc636bb764ab5c939176648703e8a
Andrei Martins <amartins13@outlook.com>
2018-09-15 16:34:10 -0300

    :sparkles: Non-personalized recommendations done

--- non_personalized.R
+++ non_personalized.R
@@ -3,29 +3,50 @@ library(dplyr)
 
 ratings <- read.csv("data/HW1-data.csv")
 
-movie_mean_rating <- function(movieId){
-  movieId <- paste0("X", movieId)
-  movie_ratings <- Filter(function(x) !is.na(x), ratings[, movieId])
+movie_mean_rating <- function(x){
+  movieId <- paste0("X", x)
+  movie_ratings <- pull(ratings, !!movieId)
   mean(movie_ratings, na.rm = TRUE)
 }
 
-movie_rating_count <- function(movieId){
-  movieId <- paste0("X", movieId)
+movie_rating_count <- function(x){
+  movieId <- paste0("X", x)
   movie_ratings <- Filter(function(x) !is.na(x), ratings[, movieId])
   length(movie_ratings)
 }
 
-movie_above_four_percentage <- function(movieId){
-  movieId <- paste0("X", movieId)
+movie_above_four_percentage <- function(x){
+  movieId <- paste0("X", x)
   movie_ratings <- Filter(function(x) !is.na(x), ratings[, movieId])
   length(which(movie_ratings >= 4)) / length(movie_ratings)
 }
 
-movies_correlation <- function(m1, m2){
-  m1 <- paste0("X", m1)
-  m2 <- paste0("X", m2)
-  m1_ratings <- ratings[, m1]
-  m2_ratings <- ratings[, m2]
+movies_association <- function(x, y){
+  x <- paste0("X", x)
+  y <- paste0("X", y)
+  x_ratings <- ratings[, x]
+  y_ratings <- ratings[, y]
+  round(length(which(x_ratings & y_ratings) == TRUE) / length(which(!is.na(x_ratings))), 3)
+}
+
+movies_correlation <- function(x, y){
+  x <- paste0("X", x)
+  y <- paste0("X", y)
+  m1_ratings <- ratings[, x]
+  m2_ratings <- ratings[, y]
   intersection <- intersect(which(!is.na(m1_ratings)), which(!is.na(m2_ratings)))
   round(cor(m1_ratings[intersection], m2_ratings[intersection]), 3)
+}
+
+mean_rating_by_gender <- function(x, gender){
+  movieId <- paste0("X", x)
+  gender_ratings <- filter(ratings, Gender..1..F..0.M. == gender) %>% pull(!!movieId)
+  mean(gender_ratings, na.rm = T)
+}
+
+percentage_above_four_by_gender <- function(x, gender){
+  movieId <- paste0("X", x)
+  gender_ratings <- filter(ratings, Gender..1..F..0.M. == gender) %>% pull(!!movieId)
+  gender_ratings <- gender_ratings[!is.na(gender_ratings)]
+  length(which(gender_ratings >= 4)) / length(gender_ratings)
 }
